e1c3be43dea7f18d5f5f5e25e5e56aafdba6701b
JHUMACHANDA <140538186+JHUMACHANDA@users.noreply.github.com>
2026-06-09 16:18:19 +0600

    Update Problem3.py

--- Task 2/Problem3.py
+++ Task 2/Problem3.py
@@ -1,1 +1,18 @@
 
+pattern = input()
+
+com = ""
+
+for ch in pattern:
+    if ch == "A":
+        com += "T"
+    elif ch == "T":
+        com += "A"
+    elif ch == "C":
+        com += "G"
+    elif ch == "G":
+        com += "C"
+
+reverse_complement = com[::-1]
+
+print(reverse_complement)
