1534aab8dd50663100ddb4954aefd7e06c60ea14
JHUMACHANDA <140538186+JHUMACHANDA@users.noreply.github.com>
2026-06-09 16:09:58 +0600

    Update Problem2.py

--- Task 2/Problem2.py
+++ Task 2/Problem2.py
@@ -1,1 +1,23 @@
 
+text = input()  
+k = int(input())
+c = {}
+
+for i in range (len(text)- k + 1 ):
+    patt = text[i:i+k]
+    
+    if patt in c:
+        c[patt]+=1
+    else:
+        c[patt]=1
+        
+        max_c=0
+        for value in c.values():
+            if value > max_c:
+                max_c=value
+       
+        
+for key in c:
+    if c[key] == max_c:
+        print(key, end=" ")
+       
